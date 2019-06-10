d64f4cfb096303c7f7ba7bb9e4617a5ae6ef747a
Advay Pal <advaypal@hotmail.com>
2019-06-10 15:40:54 +0800

    Refactor server code to use ES6

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 
 //mongoose file must be loaded before all other files in order to provide
 // models to other modules
-var mongoose = require("./mongoose"),
+const mongoose = require("./mongoose"),
   passport = require("passport"),
   express = require("express"),
   jwt = require("jsonwebtoken"),
@@ -11,8 +11,7 @@ var mongoose = require("./mongoose"),
   cors = require("cors"),
   path = require("path"),
   bodyParser = require("body-parser"),
-  request = require("request"),
-  twitterConfig = require("./twitter.config.js");
+  request = require("request");
 
 const { promisify } = require("util");
 const {
@@ -25,16 +24,16 @@ const { getTweetData } = require("./twitter");
 
 mongoose();
 
-var User = require("mongoose").model("User");
-var passportConfig = require("./passport");
+const User = require("mongoose").model("User");
+const passportConfig = require("./passport");
 
 //setup configuration for facebook login
 passportConfig();
 
-var app = express();
+const app = express();
 
 // enable cors
-var corsOption = {
+const corsOption = {
   origin: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
@@ -42,6 +41,8 @@ var corsOption = {
 };
 app.use(cors(corsOption));
 
+
+// app.options('*', cors());
 //rest API requirements
 app.use(
   bodyParser.urlencoded({
@@ -50,12 +51,12 @@ app.use(
 );
 app.use(bodyParser.json());
 
-router.route("/health-check").get(function(req, res) {
+router.route("/health-check").get((req, res) => {
   res.status(200);
   res.send("Hello World");
 });
 
-var createToken = function(auth) {
+const createToken = (auth) => {
   return jwt.sign(
     {
       id: auth.id
@@ -67,12 +68,12 @@ var createToken = function(auth) {
   );
 };
 
-var generateToken = function(req, res, next) {
+const generateToken = (req, res, next) => {
   req.token = createToken(req.auth);
   return next();
 };
 
-var sendToken = function(req, res) {
+const sendToken = (req, res) => {
   res.setHeader("x-auth-token", req.token);
   const { screen_name: username } = req.body;
   const { _id: id } = req.user;
@@ -81,26 +82,32 @@ var sendToken = function(req, res) {
   return res.status(200).send(JSON.stringify(payload));
 };
 
-router.route("/auth/twitter/reverse").post(function(req, res) {
+router.route("/auth/twitter/reverse").post((req, res) => {
   request.post(
     {
       url: "https://api.twitter.com/oauth/request_token",
       oauth: {
-        oauth_callback: "http%3A%2F%2Flocalhost%3A4000%2Ftwitter-callback",
-        consumer_key: twitterConfig.consumerKey,
-        consumer_secret: twitterConfig.consumerSecret
+        // NEED TO FIND WAY TO GET 127.0.0.1:4000 by code
+        callback: "http://127.0.0.1:4000/auth/twitter-callback",
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET
       }
     },
-    function(err, r, body) {
+    (err, r, body) => {
       if (err) {
         return res.send(500, { message: e.message });
       }
 
-      var jsonStr =
+      const jsonStr =
         '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
       res.send(JSON.parse(jsonStr));
     }
   );
+});
+
+router.route('/auth/twitter-callback').get((req, res, next) => {
+  console.log(req.query.oauth_token);
+  console.log(req.query.oauth_verifier);
 });
 
 router.route("/auth/twitter").post(
@@ -109,8 +116,8 @@ router.route("/auth/twitter").post(
       {
         url: `https://api.twitter.com/oauth/access_token?oauth_verifier`,
         oauth: {
-          consumer_key: twitterConfig.consumerKey,
-          consumer_secret: twitterConfig.consumerSecret,
+          consumer_key: process.env.TWITTER_CONSUMER_KEY,
+          consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
           token: req.query.oauth_token
         },
         form: { oauth_verifier: req.query.oauth_verifier }
@@ -134,7 +141,7 @@ router.route("/auth/twitter").post(
     );
   },
   passport.authenticate("twitter-token", { session: false }),
-  function(req, res, next) {
+  (req, res, next) => {
     if (!req.user) {
       return res.send(401, "User Not Authenticated");
     }
@@ -154,10 +161,10 @@ router.route("/auth/twitter").post(
 );
 
 //token handling middleware
-var authenticate = expressJwt({
+const authenticate = expressJwt({
   secret: "my-secret",
   requestProperty: "auth",
-  getToken: function(req) {
+  getToken: (req) => {
     if (req.headers["x-auth-token"]) {
       return req.headers["x-auth-token"];
     }
@@ -165,8 +172,8 @@ var authenticate = expressJwt({
   }
 });
 
-var getCurrentUser = function(req, res, next) {
-  User.findById(req.auth.id, function(err, user) {
+const getCurrentUser = (req, res, next) => {
+  User.findById(req.auth.id, (err, user) => {
     if (err) {
       next(err);
     } else {
@@ -176,8 +183,8 @@ var getCurrentUser = function(req, res, next) {
   });
 };
 
-var getOne = function(req, res) {
-  var user = req.user.toObject();
+const getOne = (req, res) => {
+  const user = req.user.toObject();
 
   delete user["twitterProvider"];
   delete user["__v"];
@@ -185,7 +192,7 @@ var getOne = function(req, res) {
   res.json(user);
 };
 
-async function verifyTwitterToken(req, res, next) {
+const verifyTwitterToken = async (req, res, next) => {
   const { username, twitterToken } = req.body;
   const users = await User.find({
     "twitterProvider.username": username,
@@ -196,7 +203,7 @@ async function verifyTwitterToken(req, res, next) {
   } else {
     throw new Error("Token does not exist");
   }
-}
+};
 
 router.route("/auth/me").get(authenticate, getCurrentUser, getOne);
 
@@ -232,7 +239,7 @@ router
   .route("/request-funds")
   .post(authenticate, verifyTwitterToken, fulfillFundsRequest);
 
-async function fulfillSubmitTweet(req, res, next) {
+const fulfillSubmitTweet = async (req, res, next) => {
   const { txnId, username, twitterToken } = req.body;
 
   try {
@@ -254,24 +261,24 @@ async function fulfillSubmitTweet(req, res, next) {
     console.error(e);
     res.status(400).send("Tweet not valid");
   }
-}
+};
 
 router
   .route("/submit-tweet")
   .post(authenticate, verifyTwitterToken, fulfillSubmitTweet);
 // router.route("/submit-tweet").post(authenticate, fulfillSubmitTweet);
 
-router.route("/authenticate").post(authenticate, function(req, res, next) {
+router.route("/authenticate").post(authenticate, (req, res, next) => {
   res.status(200).send(JSON.stringify("success"));
 });
 
 app.use("/", router);
 app.use("/api/v1", router);
 
-// const frontendBuild = path.join(__dirname, "frontend", "build");
-// console.log(frontendBuild);
+const frontendBuild = path.join(__dirname, "frontend", "build");
+console.log(frontendBuild);
 
-// app.use(express.static(frontendBuild));
+app.use(express.static(frontendBuild));
 
 app.listen(4000);
 module.exports = app;
