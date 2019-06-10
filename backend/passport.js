5f196483e82cc4d73bc7df92ed9f58295ca66f96
Advay Pal <advaypal@hotmail.com>
2019-06-10 12:06:21 +0800

    Update twitter config & migrate to env variable

--- backend/passport.js
+++ backend/passport.js
@@ -1,16 +1,17 @@
 "use strict";
 
-var passport = require("passport"),
+const passport = require("passport"), 
   TwitterTokenStrategy = require("passport-twitter-token"),
   User = require("mongoose").model("User"),
-  twitterConfig = require("./twitter.config.js");
+  TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET = process.env.TWITTER_CONSUMER_SECRET;;
 
 module.exports = function() {
   passport.use(
     new TwitterTokenStrategy(
       {
-        consumerKey: twitterConfig.consumerKey,
-        consumerSecret: twitterConfig.consumerSecret
+        consumerKey: TWITTER_CONSUMER_KEY,
+        consumerSecret: TWITTER_CONSUMER_SECRET
         // includeEmail: true
       },
       function(token, tokenSecret, profile, done) {
