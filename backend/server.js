ed3a85903e1860e8a636a2610388022570c8820f
Advay Pal <advaypal@hotmail.com>
2019-06-12 13:55:26 +0800

    Add sameSite to prevent against CSRF

--- backend/server.js
+++ backend/server.js
@@ -76,7 +76,7 @@ const generateAndSendToken = (req, res) => {
   const { _id: id } = req.user;
   const username = req.user.twitterProvider.username;
   const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: 24 * 60 * 60 });
-  res.cookie("token", token, {httpOnly: true });
+  res.cookie("token", token, {httpOnly: true, sameSite: true });
   res.status(200).send(JSON.stringify({ username }));
 };
 
